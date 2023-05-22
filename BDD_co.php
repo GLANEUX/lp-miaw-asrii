e03fadc5f7406aad104e2e2db4d001170ed41875
Océane GLANEUX <o.glaneux@gmail.com>
2023-05-22 14:38:54 +0200

    test gitignore

--- BDD_co.php
+++ BDD_co.php
@@ -6,9 +6,4 @@
             //On établit la connexion
             $conn = new mysqli($servername, $username, $password);
             
-            //On vérifie la connexion
-            if($conn->connect_error){
-                die('Erreur : ' .$conn->connect_error);
-            }
-            echo 'Connexion réussie';
         ?>
