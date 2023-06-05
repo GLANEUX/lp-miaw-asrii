a723cd860e7715a626a5ac8f9c60632f3f4fc2fe
Océane GLANEUX <o.glaneux@gmail.com>
2023-06-05 12:35:50 +0200

    Banner

--- view/home.php
+++ view/home.php
@@ -1,2 +1,11 @@
-<h1>Bienvenue sur notre site !</h1>
+
 <!-- Contenu de la page d'accueil -->
+<section class="banner background-image" style="background-image: url('public/img/test.png')">
+  
+  <div class="banner-overlay " ></div>
+  <div class="banner-content" >
+    <h1 class="">ASRII</h1>
+    <h2 class="">PRÉSENTATION</h2>
+  </div>
+</section>
+<h1>Bienvenue sur notre site !</h1>
