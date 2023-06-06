439fa5970ff16a4b5fba80696fb8b0eb895cf91c
Océane GLANEUX <o.glaneux@gmail.com>
2023-06-06 12:26:48 +0200

    Merge commit '3541fcdb105e8f3ce25c32a41aa038ff035771e9'

---
+++ config.php
@@ -0,0 +1,12 @@
+
+<?php
+
+// Configurer l'index du serveur
+define('URL', '/lp-miaw-asrii');
+
+// Configuration de la base de données
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_NAME', 'test');
+define('DB_PORT', 3306);
