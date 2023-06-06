d32e14ee53f12697b19b55523ce82774be3ff932
Yanelle <audreygodjo@gmail.com>
2023-06-06 15:07:57 +0200

    formations

--- view/formations.php
+++ view/formations.php
@@ -1,1 +1,71 @@
-<h1>Test</h1>
+<section class="banner background-image" style="background-image: url('public/img/test.png')">
+  <div class="banner-overlay " ></div>
+  <div class="banner-content" >
+    <h1 class="">ASRII</h1>
+    <h2 class="text-font">FORMATION</h2>
+  </div>
+</section>
+<div class="container py-5 d-flex">
+    <!-- Main content py-3 px-4 rounded-->
+    <div class="main-content col lg-9">
+        <ul class="nav nav-pills flex-column flex-sm-row" id="myTabs">
+            <li class="nav-item mx-5">
+                <a class="flex-sm-fill text-sm-center nav-link active py-3 px-4" id="presentation-tab" data-toggle="tab" href="#presentation">Présentation</a>
+            </li>
+            <li class="nav-item mx-5">
+                <a class="flex-sm-fill text-sm-center nav-link py-3 px-4" id="enseignement-tab" data-toggle="tab" href="#enseignement">Enseignement</a>
+            </li>
+        </ul>
+        <div class="tab-content" id="myTabContent">
+            <div class="tab-pane fade show active p-5" id="presentation">
+                <h4 class="">Pour y accéder</h4>
+                <p class="text-justify">Le BUT Gestion des Entreprises et des Administrations (GEA) se prépare dans un Institut Universitaire de Technologie (IUT), 
+                    en 3 ans et est organisé en unités d'enseignement (UE) capitalisables, facilement évaluables en unités de compte européennes 
+                    (ECTS), avec un découpage en six semestres ouvrant droit à l'attribution de 30 ECTS chacun.
+                </p>
+
+                <h4>Programme</h4>
+                <ul>
+                    <li>Sécurité Réseaux et des Applications</li>
+                    <li>Interconnexion des réseaux locaux</li>
+                    <li>Administration et configuration des Services intranet et internet (Windows / Linux)</li>
+                    <li>Programmation systèmes / Supervision</li>
+                    <li>Projets tutorés</li>
+                </ul>
+
+                <h4>Objectifs de la formation</h4>
+                <p class="text-justify">La Licence Métiers de l'Informatique: Applications Web offre un parcours spécifique, ASRII, qui vise à former des jeunes de 
+                    niveau BAC+2 aux métiers de l'Internet et de responsable informatique. Cette formation ASRII a des objectifs scientifiques et 
+                    professionnels clairement définis et s'inscrit dans l'offre globale de l'établissement en tant que parcours distinct de la 
+                    formation MIAW DAW2I.
+                </p>
+
+                <h4>Parcours</h4>
+                <p class="text-justify">Pour la formation Administration et Sécurisation des Réseaux et services Internet et Intranet (ASR2I), il est chargé de 
+                    l'administration et la gestion d'un parc informatique, la supervision et la sécurisation d'un réseau local, et de l'intégration 
+                    de produits et de services Intranet ou/et Internet. Les postes visés sont intermédiaires entre des techniciens supérieurs et des 
+                    ingénieurs. Savoir-faire et compétences à l'issue de ce parcours:
+                </p>
+                <ul>
+                    <li class="text-justify">Modéliser des données pour mieux intégrer les services web, concevoir et réaliser des sites web Intranet et Internet, CMS, programmer côté client et serveur.</li>
+                    <li class="text-justify">Configurer un réseau local pour un ensemble de postes informatiques, dans un environnement hétérogène (Windows, Linux) et assurer l'interconnexion avec le monde extérieur.</li>
+                    <li class="text-justify">Administrer au quotidien un parc hétérogène de postes serveurs et bureautiques rattachés au réseau informatique pour garantir, en permanence, leur disponibilité et leur sécurité.</li>
+                    <li class="text-justify">Intégrer des solutions de sécurité des applications et des données.</li>
+                </ul>
+            </div>
+            <div class="tab-pane fade p-5" id="enseignement">
+                <h1>Enseignement</h1>
+                <p>Contenu de la section 2.</p>
+            </div>
+        </div>
+    </div>
+    <!-- sidebar -->
+    <div class="sidebar bg-custom-black col-lg-3">
+        <h5 class="text-uppercase text-center">Informations</h5>
+        <div class="button bg-dark"><a href="#">Brochure</a></div>
+    </div>
+</div>
+
+<!-- Inclure les fichiers JavaScript de Bootstrap (jQuery requis) -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
