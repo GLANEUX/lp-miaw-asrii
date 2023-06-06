a10879362cce143e6fd517211bc36eb068956031
YvesMu <EternalOfIce@gmail.com>
2023-06-06 14:45:35 +0200

    Footer V2

---
+++ view/footer.php
@@ -0,0 +1,120 @@
+<!-- Footer -->
+
+
+  <!-- Footer LIENS -->
+  <div style="background: #101820;text-align:center">
+
+    <!-- Grid row -->
+    <div class="row">
+
+      <!-- Grid column -->
+      <div class="col-md-3 mx-auto">
+
+        <!-- LIENS -->
+        <h3 class="font-weight-bold text-uppercase mt-3 mb-4" style="color:white;"></h3>
+
+        <ul class="list-unstyled">
+          <img style="max-width:45%" src="public/img/ASRII_LOGO-FULL-2.png">
+          <img style="max-width:55%" src="public/img/ASRII_TXT-V.png">
+        </ul>
+
+      </div>
+      <!-- Grid column -->
+
+      <hr class="clearfix w-100 d-md-none">
+
+      <!-- Grid column -->
+      <div class="col-md-3 mx-auto">
+
+        <!-- LIENS -->
+        <h3 class="font-weight-bold text-uppercase mt-3 mb-4" style="color:white;padding-top:12%">LIENS</h3>
+
+        <ul class="list-unstyled">
+          <li>
+            <a class="link-light link-offset-2 link-underline link-underline-opacity-0" href="#!">Link 1</a>
+          </li>
+          <li>
+            <a class="link-light link-offset-2 link-underline link-underline-opacity-0" href="#!">Link 2</a>
+          </li>
+          <li>
+            <a class="link-light link-offset-2 link-underline link-underline-opacity-0" href="#!">Link 3</a>
+          </li>
+          <li>
+            <a class="link-light link-offset-2 link-underline link-underline-opacity-0" href="#!">Link 4</a>
+          </li>
+        </ul>
+
+      </div>
+      <!-- Grid column -->
+
+      <hr class="clearfix w-100 d-md-none">
+
+      <!-- Grid column -->
+      <div class="col-md-3 mx-auto">
+
+        <!-- LIENS -->
+        <h3 class="font-weight-bold text-uppercase mt-3 mb-4" style="color:white; padding-top:12%">LIENS</h3>
+
+        <ul class="list-unstyled">
+          <li>
+            <a class="link-light link-offset-2 link-underline link-underline-opacity-0" href="#!">Link 1</a>
+          </li>
+          <li>
+            <a class="link-light link-offset-2 link-underline link-underline-opacity-0" href="#!">Link 2</a>
+          </li>
+          <li>
+            <a class="link-light link-offset-2 link-underline link-underline-opacity-0" href="#!">Link 3</a>
+          </li>
+          <li>
+            <a class="link-light link-offset-2 link-underline link-underline-opacity-0" href="#!">Link 4</a>
+          </li>
+        </ul>
+
+      </div>
+      <!-- Grid column -->
+
+      <hr class="clearfix w-100 d-md-none">
+
+      <!-- Grid column -->
+      <div class="col-md-3 mx-auto">
+
+        <!-- LIENS -->
+        <h3 class="font-weight-bold text-uppercase mt-3 mb-4" style="color:white; padding-top:12%">LIENS</h3>
+
+        <ul class="list-unstyled">
+          <li>
+            <a class="link-light link-offset-2 link-underline link-underline-opacity-0" href="#!">Link 1</a>
+          </li>
+          <li>
+            <a class="link-light link-offset-2 link-underline link-underline-opacity-0" href="#!">Link 2</a>
+          </li>
+          <li>
+            <a class="link-light link-offset-2 link-underline link-underline-opacity-0" href="#!">Link 3</a>
+          </li>
+          <li>
+            <a class="link-light link-offset-2 link-underline link-underline-opacity-0" href="#!">Link 4</a>
+          </li>
+        </ul>
+
+      </div>
+      <!-- Grid column -->
+
+    </div>
+    <!-- Grid row -->
+
+  </div>
+  <!-- Footer LIENS -->
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3" style="background-color: rgba(0, 0, 0, 0.2);">© 2022-2023 CoopTeam - Copyright
+    <a href="/"> </a>
+  </div>
+  <!-- Copyright -->
+<!-- Footer -->
+        </footer>
+<!-- Bootstrap -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+
+<script type="text/javascript" src="public/js/script.js"></script>
+    </body>
+</html>
