ca37423c84f63da40702fe4d965797ac9822bc14
Etoiroc <nico.maire@hotmail.fr>
2023-06-06 15:37:10 +0200

    connexion

--- view/connexion.php
+++ view/connexion.php
@@ -4,8 +4,8 @@
     <?php if (isset($error)) { ?>
         <p><?= $error ?></p>
     <?php } ?>
-  <input type="text" placeholder="Username"  name="email_or_username" id="email_or_username" required>
-  <input type="password" placeholder="Password" name="password" id="password" required>
+  <input type="text" placeholder="Nom d'utilisateur"  name="email_or_username" id="email_or_username" required>
+  <input type="password" placeholder="Mot de passe" name="password" id="password" required>
 <input type="submit" value="Se connecter" class="button">
 </form>
 </section>
