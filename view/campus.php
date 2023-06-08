a3d773dca745d35cbf2eef7a553c98ad8c4f8cc0
YvesMu <EternalOfIce@gmail.com>
2023-06-08 12:22:55 +0200

    Campus update

--- view/campus.php
+++ view/campus.php
@@ -9,7 +9,151 @@
 </section>
 
 <br>
+<div class="row">
+<p style="padding:5%;text-align:center">Le campus de la formation ASRII à l'Université d'Évry est riche et diversifié, à l'image des étudiantes et étudiants qui la composent. Les chercheuses, chercheurs et artistes qui y participent forment une communauté ouverte, caractérisée par une grande diversité sociale, culturelle et géographique.
+
+Situé au cœur d'Évry, ce campus dédié à la formation ASRII offre un environnement unique, avec une offre large d'activités spécifiquement adaptées à cette filière. Les étudiants bénéficient d'un cadre propice à leur développement académique et professionnel, avec des opportunités sportives, culturelles et festives qui enrichissent leur expérience tout au long de leur formation.
+</p>
+</div>
+<div class="container-fluid" style="background-color:#101820">
+    <div class="row" style="display:inline-block; margin:0;">
+        <div class="col-lg-6" style="float:left;color:white">
+          <br>
+          <h2>Etudiants</h2>
+          <p style="text-align:left">Étudiant·e à l'Université d'Évry et engagé·e dans la vie associative, sociale ou professionnelle ? Demandez la validation de compétences acquises dans ces activités (bonus de 0,5 point maximum, dispense de stage ou enseignement). Une seule validation par cycle de formation (licence, master) est possible, indépendamment de l'année universitaire. La dispense concerne les matières ou stages du deuxième semestre.</p>
+          <br>
+          <br>
+        </div>
+        <div class="col-lg-4" style="float:right">
+          <br>
+          <img src="<?= URL ?>/public/img/Formation-card-HOME.png" class="card-img-top" alt="Image 1" style="margin-bottom: 3%;">
+          <br>
+        </div>
+    </div>
+    <br>
+    <br>
+    <br>
+    <div class="row" style="display:inline-block; margin:0;">
+        <div class="col-lg-6" style="float:right;color:white">
+          <br>
+          <h2 style="text-align:right">Logements</h2>
+          <p style="text-align:right">
+L'Université d'Évry met à disposition une plateforme de logement spécialement conçue pour les ASRII. Cette plateforme permet de rechercher des locations en France et à l'étranger, de constituer des colocations et d'accéder à des chambres temporaires. Les résidences CROUS à Évry-Courcouronnes proposent des logements adaptés aux ASRII ayant des besoins spécifiques en termes de mobilité réduite.
+          </p>
+          <br>
+          <br>
+        </div>
+        <div class="col-lg-4" style="float:left">
+          <br>
+          <img src="<?= URL ?>/public/img/Formation-card-HOME.png" class="card-img-top" alt="Image 1" style="margin-bottom: 3%;">
+          <br>
+        </div>
+    </div>
+</div>
+
 <br>
+
+
+
+<!-- <div class="row">
+<p style="padding:5%;text-align:center">Le campus de la formation ASRII à l'Université d'Évry est riche et diversifié, à l'image des étudiantes et étudiants qui la composent. Les chercheuses, chercheurs et artistes qui y participent forment une communauté ouverte, caractérisée par une grande diversité sociale, culturelle et géographique.
+
+Situé au cœur d'Évry, ce campus dédié à la formation ASRII offre un environnement unique, avec une offre large d'activités spécifiquement adaptées à cette filière. Les étudiants bénéficient d'un cadre propice à leur développement académique et professionnel, avec des opportunités sportives, culturelles et festives qui enrichissent leur expérience tout au long de leur formation.
+</p>
+</div>
+<div class="container-fluid" >
+    <div class="row" style="display:flex;justify-content:space-around;text-align:center">
+        <div class="col-lg-4" style="border:4px solid; padding:3%">
+          <h2>Etudiants</h2>
+          <p>Étudiant·e à l'Université d'Évry et engagé·e dans la vie associative, sociale ou professionnelle ? Demandez la validation de compétences acquises dans ces activités (bonus de 0,5 point maximum, dispense de stage ou enseignement). Une seule validation par cycle de formation (licence, master) est possible, indépendamment de l'année universitaire. La dispense concerne les matières ou stages du deuxième semestre.</p>
+          <br>
+        <ul style="text-align:left">
+          <li>Élus étudiants dans les conseils de l'Université d'Évry ou du CROUS</li>
+          <li>Service civique</li>
+          <li>Militaires dans la réserve professionnelle</li>
+          <li>Volontaires dans les armées</li>
+          <li>Sapeurs-pompiers volontaires</li>
+          <li>Activité professionnelle : salariée, travail indépendant, entrepreneur</li>
+          <li>Bénévoles au sein d'une association régie par la loi du 1er juillet 1901</li>
+          <li>Autres engagements à examiner par la commission</li>
+        </ul>
+
+          <button type="button" class="btn btn-outline-secondary"><a href="https://sphinx4.univ-evry.fr/UEVE/Engagement_etudiant_22-23/depot_demande.htm">Candidater</a></button>
+          <br>
+        </div>
+        <div class="col-lg-4" style="border:4px solid; padding:3%">
+          <h2>Logements</h2>
+          <p>
+L'Université d'Évry met à disposition une plateforme de logement spécialement conçue pour les ASRII. Cette plateforme permet de rechercher des locations en France et à l'étranger, de constituer des colocations et d'accéder à des chambres temporaires. Les résidences CROUS à Évry-Courcouronnes proposent des logements adaptés aux ASRII ayant des besoins spécifiques en termes de mobilité réduite.
+          </p>
+          <ul style="text-align:left">
+          <li>
+            <h5>RÉSIDENCE FLORA TRISTAN</h5>
+            <ul>
+              <li>1 rue Jules Vallès, 91000 Evry-Courcouronnes</li>
+              <li>Évry-Courcouronnes (RER D)</li>
+            </ul>
+          </li>
+          <li>
+            <h5>RÉSIDENCE LES AUNETTES</h5>
+            <ul>
+              <li>1 impasse de Prédecelle, 91000 Evry-Courcouronnes</li>
+              <li>Evry-Courcouronnes (RER D)</li>
+            </ul>
+          </li>
+          <li>
+            <h5>RÉSIDENCE LE DRAGUEUR</h5>
+            <ul>
+              <li>4, allée Jean Rostand, 91000 Evry-Courcouronnes</li>
+              <li>Evry-Courcouronnes (RER D)</li>
+            </ul>
+          </li>
+          <li>
+            <h5>RÉSIDENCE MARGUERITE YOURCENAR</h5>
+            <ul>
+              <li>15, rue André Lalande, 91000 Evry-Courcouronnes</li>
+              <li>Evry-Courcouronnes (RER D)</li>
+            </ul>
+          </li>
+        </ul>
+          <br>
+          <button type="button" class="btn btn-outline-secondary">Secondary</button>
+          <br>
+        </div>
+    </div>
+</div>
 <br>
-<img src="public/img/Banner-HOME.jpg" class="rounded float-start" alt="...">
-<img src="public/img/Banner-HOME.jpg" class="rounded float-end" alt="...">
+-->
+
+<div class="container-fluid" >
+<br>
+    <div class="row" style="display:flex;justify-content:space-around;text-align:center">
+    <h1 style="margin-bottom:1%"> Restauration</h1>
+    
+        <div class="col-lg-3" style="border:4px solid; padding:2%">
+          <strong><h3>Restaurant Universitaire Monseigneur Roméro</h3></strong>
+              <p style="height: 3%;">Bâtiment des 1ers Cycles de l’université</p>
+              <p style="height: 3%;">Du lundi au vendredi de 11h30 à 14h</p>
+              <p style="height: 3%;">Self traditionnel/grillades/pizzas</p>
+          <br>
+        </div>
+        <div class="col-lg-3" style="border:4px solid; padding:2%">
+          <strong><h3>Restaurant Universitaire Le sablier</h3></strong>
+              <p style="height: 3%;">Rue du père Jarlan</p>
+              <p style="height: 3%;">Du lundi au vendredi de 11h30 à 14h</p>
+              <p style="height: 3%;">Self traditionnel/ligne express/grillade</p>
+          <br>
+        </div>
+        <div class="col-lg-3" style="border:4px solid; padding:2%">
+          <strong><h3>Restaurant Universitaire Le Boomerang</h3></strong>
+              <p style="height: 3%;">IUT de Brétigny</p>
+              <p style="height: 3%;">Du lundi au vendredi de 11h30 à 14h</p>
+              <p style="height: 3%;">Self traditionnel/Cafétéria</p>
+          <br>
+        </div>
+    </div>
+</div>
+<br>
+
+
+
