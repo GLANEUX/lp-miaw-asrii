1696c4dfd6818a45b33bf206885f4f7ca6cd881b
Etoiroc <nico.maire@hotmail.fr>
2023-06-08 15:36:00 +0200

    Page projetsAjout

--- view/projetsAjout.php
+++ view/projetsAjout.php
@@ -7,23 +7,23 @@
 <h1>Proposer un Projet Tuteuré</h1> 
   <div class="container">
     <div class="column">
-      <form action="#" method="post">
+      <form action="<?= URL ?>/projets/add" method="post">
         <label for="entreprise">Nom de l'entreprise :</label>
-        <input type="text" id="entreprise" name="entreprise" value="" required>
+        <input type="text" id="entreprise" name="entreprise" value="" placeholder="Nom de l'entreprise" required>
 
         <label for="contact">Personne de contact :</label>
-        <input type="text" id="contact" name="contact" value="" required>
+        <input type="text" id="contact" name="contact" value="" placeholder="Personne de contact" required>
 
         <label for="email">Adresse e-mail :</label>
-        <input type="text" id="email" name="email" value="" required>
+        <input type="text" id="email" name="email" value="" placeholder="Adresse e-mail" required>
       </div>
 
       <div class="column">
         <label for="telephone">Numéro de téléphone :</label>
-        <input type="text" id="telephone" name="telephone" value="" required>
+        <input type="text" id="telephone" name="telephone" value="" placeholder="Numéro de téléphone" required>
 
         <label for="projet">Description du projet :</label>
-        <textarea id="projet" name="projet" rows="5" required></textarea>
+        <textarea id="projet" name="projet" rows="5" placeholder="Description du projet" required></textarea>
 
         <input type="submit" value="Envoyer">
       </form>
